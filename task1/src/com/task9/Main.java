5a037ed6615b02237529a3f58f24b5b00c571dbd
averjan <averkintimofej@gmail.com>
2021-09-22 22:47:34 +0300

    Edit Main class for testing Bucket class

--- task1/src/com/task9/Main.java
+++ task1/src/com/task9/Main.java
@@ -1,8 +1,19 @@
 package com.task9;
 
+import com.task9.color.Color;
+import com.task9.items.Ball;
+import com.task9.items.Bucket;
+
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        Bucket bucket = new Bucket();
+        bucket.add(new Ball(12, Color.RED));
+        bucket.add(new Ball(40, Color.BLUE));
+        bucket.add(new Ball(2, Color.RED));
+        bucket.add(new Ball(1, Color.BLUE));
+
+        System.out.println(bucket.itemsWeight());
+        System.out.println((long) bucket.filterBallsByColor(Color.BLUE).size());
     }
 }
--- task1/src/com/task9/items/Ball.java
+++ task1/src/com/task9/items/Ball.java
@@ -3,6 +3,14 @@ package com.task9.items;
 import com.task9.color.Color;
 
 public class Ball {
-    public int weight = 0;
+    public int weight = 1;
     public Color color = Color.WHITE;
+
+    public Ball() {
+    }
+
+    public Ball(int weight, Color color) {
+        this.weight = weight;
+        this.color = color;
+    }
 }
