734f4ae7044d17350848d9cc03bebfd9b384fae1
averjan <averkintimofej@gmail.com>
2021-09-22 22:20:30 +0300

    Add Bucket class

---
+++ task1/src/com/task9/items/Ball.java
@@ -0,0 +1,8 @@
+package com.task9.items;
+
+import com.task9.color.Color;
+
+public class Ball {
+    public int weight = 0;
+    public Color color = Color.WHITE;
+}
---
+++ task1/src/com/task9/items/Bucket.java
@@ -0,0 +1,7 @@
+package com.task9.items;
+
+import java.util.ArrayList;
+
+public class Bucket {
+    ArrayList<Ball> balls = new ArrayList<>();
+}
