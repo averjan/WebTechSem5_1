0f32c96df377e95a82b2edcc9662af8aff56510c
averjan <averkintimofej@gmail.com>
2021-09-25 02:10:31 +0300

    Add test code for Book class to Main class

--- task1/src/com/task12/Main.java
+++ task1/src/com/task12/Main.java
@@ -1,8 +1,18 @@
 package com.task12;
 
+import com.task12.book.Book;
+
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        Book book = new Book("The captain's daughter", "Pushkin", 3);
+        Book book2 = new Book("The captain's daughter", "Pushkin", 3);
+        Book book3 = new Book("War and Piece", "Tolstoy", 10);
+        System.out.println(book.equals(book));
+        System.out.println(book.equals(12));
+        System.out.println(book.equals(book2));
+        System.out.println(book.equals(book3));
+        System.out.println(book.toString());
+        System.out.println(book.hashCode());
     }
 }
--- task1/src/com/task12/book/Book.java
+++ task1/src/com/task12/book/Book.java
@@ -4,7 +4,13 @@ public class Book {
     private String title;
     private String author;
     private int price;
-    private static int edition;
+    private static int edition = 12;
+
+    public Book(String title, String author, int price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
 
     @Override
     public boolean equals(Object anotherObject) {
